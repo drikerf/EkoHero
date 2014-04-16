c43e18ed09283f43dd27a7a7223bc025fda858d9
Fredrik <fredrik@n173-p28.kthopen.kth.se>
2014-04-16 09:21:50 +0200

    init layout

--- EkoHero/index.php
+++ EkoHero/index.php
@@ -1,3 +1,17 @@
-<?php
-    echo "Hello Eko Heroes";
-?>
+<!doctype html>
+<html>
+<head>
+    <title>EkoHero</title>
+    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="resources/css/main.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <h1>EkoHero</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
